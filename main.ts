67fc58ea603759bf813c7ca24ecc4de308adc1fa
Fabio Nicoliello <32755862+fabionicoliellofab@users.noreply.github.com>
2025-06-15 02:39:40 -0300

    BME280 sensors

--- main.ts
+++ main.ts
@@ -1,23 +1,15 @@
-let humedad = 0
-let temperatura = 0
+let Humedad = 0
+let Temp = 0
 let presion = 0
-datalogger.setColumnTitles(
-"presion",
-"temperatura",
-"humedad"
-)
+radio.setGroup(1)
+BME280.PowerOn()
 BME280.Address(BME280_I2C_ADDRESS.ADDR_0x76)
-loops.everyInterval(600000, function () {
+loops.everyInterval(60000, function () {
     presion = BME280.pressure(BME280_P.hPa)
-    basic.showNumber(presion)
-    temperatura = BME280.temperature(BME280_T.T_C)
-    basic.showNumber(temperatura)
-    humedad = BME280.humidity()
-    basic.showNumber(humedad)
-    datalogger.log(
-    datalogger.createCV("presion", presion),
-    datalogger.createCV("temperatura", temperatura),
-    datalogger.createCV("humedad", humedad)
-    )
+    radio.sendValue("Presion", presion)
+    Temp = BME280.temperature(BME280_T.T_C)
+    radio.sendValue("Temp", Temp)
+    Humedad = BME280.humidity()
+    radio.sendValue("Humedad", Humedad)
     basic.pause(2000)
 })
